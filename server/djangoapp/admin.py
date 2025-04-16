e25fb27d9da0bdf63c7ab39d7ab1516ad506fdce
Harsha <vardhanharsha607@gmail.com>
2025-04-16 03:10:27 -0400

    added car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
-
-# Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1  # Number of empty CarModel forms to display in CarMake admin
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'year', 'car_make']
+    list_filter = ['type', 'year', 'car_make']
+    search_fields = ['name', 'car_make__name']
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,16 +1,23 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # path for registration
     path('register/', views.registration, name='register'),
+
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    path('login', views.login_user, name='login'),
+
+    # path for logout
     path('logout/', views.logout_request, name='logout'),
+
+    # path for getting cars
+    path('get_cars/', views.get_cars, name='get_cars'),
+
     # path for dealer reviews view
 
     # path for add a review view
